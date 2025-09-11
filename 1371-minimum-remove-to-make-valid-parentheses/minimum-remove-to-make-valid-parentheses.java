53f61a7fcc0e479c1ba2f91c2f73b738f1740c9c
Eragolla Akhilesh <143899297+akhilesheragolla2056@users.noreply.github.com>
2025-09-11 14:53:08 +0530

    Time: 606 ms (5.04%) | Memory: 46.4 MB (8.15%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
@@ -1,48 +1,37 @@
-
 class Solution {
     public String minRemoveToMakeValid(String s) {
-        // Initialize pointers for the start and end of the string
-        int startPointer = 0;
-        int endPointer = s.length() - 1;
-
-        String result;
-
-        // Convert input string to character array for easier manipulation
-        char[] arr = s.toCharArray();
-        
-        // Counter for open parentheses
-        int openParenthesesCount = 0;
-
-        // First pass: mark excess closing parentheses with '*'
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == '(')
-                openParenthesesCount++;
-            else if (arr[i] == ')') {
-                if (openParenthesesCount == 0)
-                    arr[i] = '*'; // Mark excess closing parentheses
-                else
-                    openParenthesesCount--;
+        int open=0,close=0,flag=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='(') {
+                open++;
+                flag+=1;
+            }
+            else if(s.charAt(i)==')'&&flag>0) {
+                close++;
+                flag--;
             }
         }
-
-        // Second pass: mark excess opening parentheses from the end
-        for (int i = arr.length - 1; i >= 0; i--) {
-            if (openParenthesesCount > 0 && arr[i] == '(') {
-                arr[i] = '*'; // Mark excess opening parentheses
-                openParenthesesCount--;
+        int k=Math.min(open,close);
+        String ans="";
+        open=k;
+        close=k;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='('){
+                if(open>0) {
+                    ans+='(';
+                    open--;
+                }
+                continue;
+            } 
+            if(s.charAt(i)==')') {
+               if(close>0&&close>open) {
+                  ans+=')'; 
+                  close--;
+               }
+               continue;
             }
+            else ans+=s.charAt(i);
         }
-        
-        // Filter out marked characters and store the result in the character array
-        int p = 0; // Pointer for updating the character array
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] != '*')
-                arr[p++] = arr[i];
-        }
-
-        // Construct the result string from the filtered character array
-        result = new String(arr).substring(0, p);
-
-        return result;
+        return ans;
     }
 }
